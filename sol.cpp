a2e7c13e7462db2e53869e3471d1aa568bd7a18b
erroer404 <91798713+erroer404@users.noreply.github.com>
2022-10-02 13:48:35 +0530

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -8,7 +8,9 @@ int32_t main()
     cin.tie(NULL),cout.tie(NULL);
     int t;   cin>>t;
     assert(t<=10);
+    
     assert(t>=1);
+    
     while(t--){
         int fre[N]={0};
         int n;     cin>>n;
@@ -32,28 +34,5 @@ int32_t main()
         }
         cout<<s<<"\n";
     }
-    return 0;
-    while(t--){
-        int fre[N]={0};
-        int n;     cin>>n;
-        int a[n];
-        assert(n<=100000);
-        assert(n>=1);
-        for(int i=0;i<n;++i){
-            cin>>a[i];
-            assert(a[i]<=1000000);
-            assert(a[i]>0);
-        }
-        sort(a,a+n);
-        int s=0;
-        for(int i=0;i<n;i++)
-        {
-            int d=fre[a[i]];
-            int u=(d*(d-1))/2;
-            s+=u;
-            for(int j=a[i]+a[i];j<N;j+=a[i])
-                fre[j]++;
-        }
-        cout<<s<<"\n";
-    }
+    
 }
